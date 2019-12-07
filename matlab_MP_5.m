f318b38a22a9db45aea64e3824be9d9777313d18
_beaverabellera <47809705+joshuabeaver@users.noreply.github.com>
2019-12-08 02:09:33 +0800

    Add files via upload

---
+++ matlab_MP_5.m
@@ -0,0 +1,23 @@
+function[] = matlab_MP_5()
+clc
+n = 0:199;
+x = input('Enter function x(n):  ');
+y = 0:199;
+
+for counter = 1:1:200
+    if counter == 1
+        y(counter) = -1.5*x(counter) + 2*x(counter+1) -0.5*x(counter+2);
+    elseif counter>1 && counter<=199
+        y(counter) = 0.5*x(counter+1) -0.5*x(counter-1);
+    elseif counter == 200
+        y(counter) = 1.5*x(counter) -2*x(counter-1) +0.5*x(counter-2);
+    end
+end
+plot(n,x,'linewidth',3)
+hold on
+plot(n,y,'linewidth',3)
+legend('x(n)','y(n)')
+grid on
+hold off
+end
+    
